0346e913755dc91108467f9397ec506dcd9b298f
David Churchill <dchurchill92@gmail.com>
2015-09-21 18:22:14 -0500

    add placeholder templates and routing

--- client/app/app.js
+++ client/app/app.js
@@ -3,4 +3,19 @@ angular.module('soundbase', [
   'soundbase.tracks',
   'soundbase.upload',
   'ngRoute'
-]);
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/tracks', {
+      templateUrl: 'app/tracks/tracks.html',
+      controller: 'TracksController',
+    })
+    .when('/upload', {
+      templateUrl: 'app/upload/upload.html',
+      controller: 'UploadController',
+    })
+    .otherwise({
+      redirectTo: '/tracks'
+    });
+}]);
